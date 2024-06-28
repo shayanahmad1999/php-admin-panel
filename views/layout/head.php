3641a38924ea6a99846644bcdc957b0f434aac45
Shayan Ahmad <shayanahmad235@gmail.com>
2024-06-28 19:53:37 +0500

    head.php updated

--- views/layout/head.php
+++ views/layout/head.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Dashboard One | Notika - Notika Admin Template</title>
+    <title><?= title('HOME') ?></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon
