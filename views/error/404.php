31281f9d6338e91930d61d917872be64bc8dbe4f
Shayan Ahmad <shayanahmad235@gmail.com>
2024-06-28 19:53:14 +0500

    create error page if page not found

---
+++ views/error/404.php
@@ -0,0 +1,10 @@
+
+<div class="error-page-area">
+    <div class="error-page-wrap">
+        <i class="notika-icon notika-close"></i>
+        <h2>ERROR <span class="counter">404</span></h2>
+        <p>Sorry, but the page you are looking for has note been found. Try checking the URL for an error, then hit the refresh button on your browser or try found something else in our app.</p>
+        <a href="index.html" class="btn">Dashboard</a>
+        <a href="#" class="btn error-btn-mg">Report Problem</a>
+    </div>
+</div>
