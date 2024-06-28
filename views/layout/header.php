70feb12421c09806e10040dcff6c0ea1bbc2efee
Shayan Ahmad <shayanahmad235@gmail.com>
2024-06-28 23:25:59 +0500

    add new routes

--- views/layout/header.php
+++ views/layout/header.php
@@ -11,6 +11,12 @@
                                         <li><a href="<?= url('index')?>">Dashboard</a></li>
                                     </ul>
                                 </li>
+                                <li><a data-toggle="collapse" data-target="#Charts" href="#">Tables</a>
+                                    <ul class="collapse dropdown-header-top">
+                                        <li><a href="<?= url('normaltable')?>">Normal Table</a></li>
+                                        <li><a href="<?= url('datatable')?>">Data Table</a></li>
+                                    </ul>
+                                </li>
                             </ul>
                         </nav>
                     </div>
@@ -25,19 +31,17 @@
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <ul class="nav nav-tabs notika-menu-wrap menu-it-icon-pro">
-                        <li class="active"><a href="<?= url('index')?>"><i class="notika-icon notika-house"></i> Dashboard</a>
+                        <li class="active"><a href="<?= url('dashboard')?>"><i class="notika-icon notika-house"></i> Dashboard</a>
                         </li>
-                        <li><a data-toggle="tab" href="#mailbox"><i class="notika-icon notika-mail"></i> Email</a>
+                        <li><a data-toggle="tab" href="#mailbox"><i class="notika-icon notika-mail"></i> Tables</a>
                         </li>
                     </ul>
                     <div class="tab-content custom-menu-content">
                         <div id="mailbox" class="tab-pane notika-tab-menu-bg animated flipInX">
                             <ul class="notika-main-menu-dropdown">
-                                <li><a href="inbox.html">Inbox</a>
+                                <li><a href="<?= url('normaltable') ?>">Normal table</a>
                                 </li>
-                                <li><a href="view-email.html">View Email</a>
-                                </li>
-                                <li><a href="compose-email.html">Compose Email</a>
+                                <li><a href="<?= url('datatable') ?>">Data table</a>
                                 </li>
                             </ul>
                         </div>
