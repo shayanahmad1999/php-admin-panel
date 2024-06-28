4a78ad6e20c3a58977ed22d79cef05c4bb53b372
Shayan Ahmad <shayanahmad235@gmail.com>
2024-06-29 00:40:51 +0500

    add new routes in switch statement

--- index.php
+++ index.php
@@ -27,6 +27,50 @@ if (file_exists($controllerFile)) {
             $pageTitle = "Data Table";
             $dashboardController->datatable();
             break;
+        case 'form1':
+            $pageTitle = "Forms 1";
+            $dashboardController->form1();
+            break;
+        case 'form2':
+            $pageTitle = "Forms 2";
+            $dashboardController->form2();
+            break;
+        case 'form3':
+            $pageTitle = "Forms 3";
+            $dashboardController->form3();
+            break;
+        case 'notifications':
+            $pageTitle = "Notifications";
+            $dashboardController->notifications();
+            break;
+        case 'alerts':
+            $pageTitle = "alerts";
+            $dashboardController->alerts();
+            break;
+        case 'modals':
+            $pageTitle = "modals";
+            $dashboardController->modals();
+            break;
+        case 'buttons':
+            $pageTitle = "buttons";
+            $dashboardController->buttons();
+            break;
+        case 'tabs':
+            $pageTitle = "tabs";
+            $dashboardController->tabs();
+            break;
+        case 'accordian':
+            $pageTitle = "accordian";
+            $dashboardController->accordian();
+            break;
+        case 'dialogs':
+            $pageTitle = "dialogs";
+            $dashboardController->dialogs();
+            break;
+        case 'dropdowns':
+            $pageTitle = "dropdowns";
+            $dashboardController->dropdowns();
+            break;
         default:
             require_once 'views/error/404.php';
             break;
