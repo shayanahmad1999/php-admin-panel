19d0e742d6a5442f49bdcfe1522d2d1b1506f305
Shayan Ahmad <shayanahmad235@gmail.com>
2024-06-28 19:54:52 +0500

    updated index file

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php 
+require_once 'includes/function.php';
 
 require_once 'views/layout/head.php';
 require_once 'views/layout/header.php';
 
-require_once 'views/dashboard.php';
+require_once 'controllers/DashboardController.php';
+
+$page = isset($_GET['page']) ? $_GET['page'] : 'dashboard';
+$page = preg_replace('/[^-a-zA-Z0-9_]/', '', $page);
+
+$controllerFile = 'controllers/' . $page . 'Controller.php';
+if (file_exists($controllerFile)) {
+    require_once $controllerFile;
+}
+else {
+    require_once 'views/error/404.php';
+}
 
 require_once 'views/layout/footer.php';
