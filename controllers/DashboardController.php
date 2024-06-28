3edf3a6635117c4e2742546b5b31990f27621304
Shayan Ahmad <shayanahmad235@gmail.com>
2024-06-29 00:41:29 +0500

    add more code in dashboard controller

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -13,6 +13,50 @@ class DashboardController{
     {
         require_once "views/tables/data-table.php";
     }
+    public function form1()
+    {
+        require_once "views/forms/form1.php";
+    }
+    public function form2()
+    {
+        require_once "views/forms/form2.php";
+    }
+    public function form3()
+    {
+        require_once "views/forms/form3.php";
+    }
+    public function notifications()
+    {
+        require_once "views/appviews/notifications.php";
+    }
+    public function alerts()
+    {
+        require_once "views/appviews/alerts.php";
+    }
+    public function modals()
+    {
+        require_once "views/appviews/modals.php";
+    }
+    public function buttons()
+    {
+        require_once "views/appviews/buttons.php";
+    }
+    public function tabs()
+    {
+        require_once "views/appviews/tabs.php";
+    }
+    public function accordian()
+    {
+        require_once "views/appviews/accordian.php";
+    }
+    public function dialogs()
+    {
+        require_once "views/appviews/dialogs.php";
+    }
+    public function dropdowns()
+    {
+        require_once "views/appviews/dropdowns.php";
+    }
 }
 
 $controller = new DashboardController();
