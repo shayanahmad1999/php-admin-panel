ebd8f5584b39aa0176dabc7c416995f19e75f436
Shayan Ahmad <shayanahmad235@gmail.com>
2024-06-28 23:24:47 +0500

    update main index file

--- index.php
+++ index.php
@@ -1,18 +1,33 @@
-<?php 
-require_once 'includes/function.php';
-
-require_once 'views/layout/head.php';
+<?php
+require_once 'includes/functions.php';
 require_once 'views/layout/header.php';
 
 $page = isset($_GET['page']) ? $_GET['page'] : 'dashboard';
+
 $page = preg_replace('/[^-a-zA-Z0-9_]/', '', $page);
 
-$controllerFile = 'controllers/' . $page . 'Controller.php';
-if (file_exists($controllerFile)) {
-    require_once $controllerFile;
+require_once 'controllers/DashboardController.php';
+
+$dashboardController = new DashboardController();
+
+switch ($page) {
+    case 'dashboard':
+        $pageTitle = "Dashboard";
+        $dashboardController->index();
+        break;
+    case 'normaltable':
+        $pageTitle = "Normal Table";
+        $dashboardController->normaltable();
+        break;
+    case 'datatable':
+        $pageTitle = "Data Table";
+        $dashboardController->datatable();
+        break;
+    default:
+        require_once 'views/error/404.php';
+        break;
 }
-else {
-    require_once 'views/error/404.php';
-}
+
+require_once 'views/layout/head.php';
 
 require_once 'views/layout/footer.php';
