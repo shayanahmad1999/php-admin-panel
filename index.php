5d31c0ddbb3797d79f8af534c3c38bd74a179b3e
Shayan Ahmad <shayanahmad235@gmail.com>
2024-06-30 19:41:56 +0500

    remove extra code

--- index.php
+++ index.php
@@ -1,22 +1,16 @@
 <?php
 require_once 'includes/functions.php';
 
-// Define pages that do not require login
-// $publicPages = ['login', 'user'];
-// if (!in_array($_GET['page'] ?? 'dashboard', $publicPages) && !isset($_SESSION['user_id'])) {
-// Check if the user is logged in
-// if (!Auth::user() && (!isset($_GET['page']) || $_GET['page'] !== 'login')) {
-//     require_once "views/auth/login.php";
-//     exit();
-// }
-
 ob_start();
 
 require_once 'routes/web.php'; 
 
 $content = ob_get_clean();
 
-require_once 'views/layout/header.php';
+if(Auth::user())
+{
+    require_once 'views/layout/header.php';
+}
 require_once 'views/layout/head.php';
 
 echo $content;
